47286c6aaa3b6ab296d2ef6f2a33327726ba8eae
Erik <Ohls2nd@gmail.com>
2018-03-04 16:47:59 +0100

    timer

--- public/js/client.js
+++ public/js/client.js
@@ -13,3 +13,24 @@ function toggleAlternativ(){
         alt_enable = true;
     }
 }
+
+// Timer. Delay = Minuter
+// <p id="ptimer"></p> för användning
+var delay = 15;
+var future = new Date().getTime() + delay*60000;
+
+var x = setInterval(function() {
+  var now = new Date().getTime();
+  var distance = future - now;
+
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("ptimer").innerHTML = + minutes + "m " + seconds + "s ";
+
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("ptimer").innerHTML = "Framme!";
+  }
+}, 1000);
